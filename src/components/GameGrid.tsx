0ea0402c4abc6b9c04c911b120d9176328352e38
Akash R Chandran <chandranrakash@gmail.com>
2023-05-31 14:18:37 +0530

    updated to support generic data type

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,22 +6,22 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const skeletons = [1, 2, 3, 4, 5, 6];
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} padding={10} spacing='20px'>
-      {isLoading &&
-        skeletons.map((skeleton) => (
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding={10} spacing={3}>
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
-      {games.map((game) => (
-        <GameCardContainer key={game.id}>
-          <GameCard game={game} />
-        </GameCardContainer>
-      ))}
       </SimpleGrid>
     </>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,33 +1,12 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-helper";
-import { FetchGamesResponse, Game } from "../models/gameAPIResponse";
-import { CanceledError } from "axios";
+import { Game } from "../models/gameAPIResponse";
+import useData from "./useData";
 
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) {
-          return;
-        }
-        setError(err);
-      });
+export interface Platform { 
+  id: number;
+  metacritic: number;
+}
 
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>('/games');
 
 export default useGames;
