fb11fa19946425c1bbc72415f88c5693c6c2524b
Akash R Chandran <chandranrakash@gmail.com>
2023-06-01 11:32:05 +0530

    saved genre data into a file

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
-import useData from "./useData";
-import { Genre } from "../models/genreAPIResponse";
+import genres from '../data/genres'
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
