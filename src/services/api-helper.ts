902a6d40063b7fd3b97737ad354ad6551e145fb2
Akash R Chandran <chandranrakash@gmail.com>
2023-06-01 14:29:03 +0530

    updated apiKey

--- src/services/api-helper.ts
+++ src/services/api-helper.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const apiKey = 'dc813b8f9f8a409e9deff9b962576948';
+
 const apiClient = axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: "dc813b8f9f8a409e9deff9b962576948",
+        key: apiKey,
     }
 });
 
