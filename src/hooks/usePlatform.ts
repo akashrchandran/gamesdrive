34819541f03fde1a51018f77bfbb31d54bd3716a
Akash R Chandran <chandranrakash@gmail.com>
2023-09-05 00:12:12 +0530

    changed to react query [ Platfroms ]

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-helper";
+import { FetchResponse } from "./useData";
+import { Platform } from "../models/gameAPIResponse";
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>('/platforms/lists/parents')
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h,
+    initialData: { count: platforms.length, results: platforms }
+  });
 
 export default usePlatforms;
